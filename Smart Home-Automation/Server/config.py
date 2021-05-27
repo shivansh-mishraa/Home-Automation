9e613fa56a9c97c22d1df7dc35bd1af42e4d96ad
Shivansh Mishra <59003039+shivansh-mishraa@users.noreply.github.com>
2021-05-27 19:52:44 +0530

    Add files via upload

---
+++ Smart Home-Automation/Server/config.py
@@ -0,0 +1,14 @@
+configs_db={
+	'user': 'root',
+	'password': 'qwerty',
+	'host': 'localhost',
+	'database': 'SmartHome'
+  }
+configs_user={
+	'username':'admin',
+	'password':'hello123'
+}
+gcm_api={
+'GOOGLE_API_KEY': 'AIzaSyAmmvVIcRKEwTDksISaiDJjsdCznWoo-eA'
+
+}
